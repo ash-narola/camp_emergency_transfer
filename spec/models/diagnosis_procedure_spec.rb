a2321825c5504d2b4b666a5736e98201a47cb727
Aakanksha <ash@narola.email>
2019-04-19 16:50:35 +0530

    Tests for controller and model

--- spec/models/diagnosis_procedure_spec.rb
+++ spec/models/diagnosis_procedure_spec.rb
@@ -4,4 +4,13 @@ require 'rails_helper'
 
 RSpec.describe DiagnosticProcedure, type: :model do
   it { is_expected.to belong_to(:patient) }
+
+  describe ".diagnostic_procedure_summary" do
+    let!(:patient) { FactoryGirl.create(:patient) }
+    let!(:diagnostic_procedure) { FactoryGirl.create(:diagnostic_procedure, patient: patient) }
+    it "gets described code" do
+      s = "#{diagnostic_procedure.description} at #{diagnostic_procedure.created_at.strftime('%H:%M')}"
+      expect(diagnostic_procedure.diagnostic_procedure_summary).to eq s
+    end
+  end
 end
--- spec/models/diagnosis_spec.rb
+++ spec/models/diagnosis_spec.rb
@@ -4,4 +4,13 @@ require 'rails_helper'
 
 RSpec.describe Diagnosis, type: :model do
   it { is_expected.to belong_to(:diagnosable) }
+
+  describe ".described_code" do
+    let!(:patient) { FactoryGirl.create(:patient) }
+    let!(:diagnosis) { FactoryGirl.create(:diagnosis, diagnosable: patient) }
+    it "gets described code" do
+      s = "#{diagnosis.description} (#{diagnosis.code})"
+      expect(diagnosis.described_code).to eq s
+    end
+  end
 end
